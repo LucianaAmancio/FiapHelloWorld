c56fe99636708cb54443b452243c542aa18ae5cb
LucianaAmancio <luciana.da.silva.abreu.amancio@hotmail.com>
2021-12-17 13:54:38 -0300

    <Capítulo 2 - C# - Tipos de variáveis>

--- FiapHelloWorld/Program.cs
+++ FiapHelloWorld/Program.cs
@@ -3,28 +3,71 @@
 
 namespace FiapHelloWorld
 {
-    
-    //Primeira classe criada no curso
+
+    //Capítulo 01 - Introdução à plataforma .NET
+
     //class Program
     //{
-       
+
     //    static void Main(string[] args)
     //    {
     //        Console.WriteLine("Fiap - Ola C#");
     //        // trecho para manter a janela aberta
     //        Console.Read();
-                  
+
     //}
 
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Models.HelloModel model = new Models.HelloModel();
-            Console.WriteLine(model.Mensagem);
-            Console.Read();
-        }
-    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        Models.HelloModel model = new Models.HelloModel();
+    //        Console.WriteLine(model.Mensagem);
+    //        Console.Read();
+    //    }
+    //}
+
+
+    //Capítulo 2 - C# - Tipos de variáveis
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int valorInt = 100;
+    //        // convertendo inteiro para long
+    //        long valorLong = valorInt;
+    //        // convertendo long para double
+    //        double valorDouble = valorLong;
+    //        // Imprimindo conteúdo das variável
+    //        Console.WriteLine("Valor Inteiro:" + valorInt);
+    //        Console.WriteLine("Valor Long:" + valorLong);
+    //        Console.WriteLine("Valor Double:" + valorDouble);
+    //        // Para a execução até o usuário teclar Enter.
+    //        Console.Read();
+    //    }
+    //}
+
+
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+    //        int valorInt = 100;
+    //        long valorLong = valorInt;
+    //        double valorDouble = valorLong;
+    //        // declaração de conversação (Parse)
+    //        valorInt = (int)valorLong;
+    //        Console.WriteLine(valorInt);
+    //        Console.WriteLine(valorLong);
+    //        Console.WriteLine(valorDouble);
+    //        Console.Read();
+    //    }
+    //}
+
+
+    //Capítulo 2 - C# -  Operadores
+
+
+
 
 }
- 
